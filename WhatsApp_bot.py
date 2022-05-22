6dcf4734558494679f56fcc89181d1996588f8bf
Aryia Behroziuan <72256856+Aryia-Behroziuan@users.noreply.github.com>
2022-05-22 14:58:19 +0430

    Update WhatsApp_bot.py

--- WhatsApp_bot.py
+++ WhatsApp_bot.py
@@ -1,34 +1,27 @@
-## for get to site
-import requests
 import time
-## for save in databace spacify
-import mysql.connector
-## other theme
-from bs4 import BeautifulSoup
 ## import selenium library
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import pyperclip
 ## web project selenium
 from selenium.webdriver.common.by import By
-## for create userId random
-from random import seed
-from random import random
-import random
+
 
 
 ## connect driver to code file with path
-#c_option = Options()
-#c_option.add_argument(r"user-data-dir=C:\\Users\\Meysam\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 4")
+c_option = Options()
+c_option.add_argument(r"user-data-dir=C:\\Users\\PROGTAMING\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1")
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://web.whatsapp.com/')
 
-run = input('enter for run program')
-name = input('/@Enter contact name: ')
+run = input('enter for run program:')
+
+# Click to contacts in whatsApp
+#contacts = driver.find_element_by_xpath('//div[@title = "New chat"]')
+#contacts.click()
+
 msg = input('/@Enter msg: ')
-count = int(input('/@How many times: '))
 
 input('Enter Anything...')
 
@@ -36,23 +29,22 @@ input('Enter Anything...')
 ##accept_cookies = driver.find_element_by_id('onetrust-accept-btn-handler')
 ##accept_cookies.click()
 
+users = driver.find_elements_by_xpath('//div[@class="lhggkp7q ln8gz9je rx9719la"]')
+for user in users:
 
+    # click contact on WhatsApp
+    user.click()
 
-# click contact on WhatsApp
-contact = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-contact.click()
+    # message box for typing with robot
+    ##msg_box = driver.find_element_by_class_name('_13NKt')
+    msg_box = driver.find_element_by_xpath('//div[@title = "Type a message"]')
 
-# message box for typing with robot
-##msg_box = driver.find_element_by_class_name('_13NKt')
-msg_box = driver.find_element_by_xpath('//div[@title = "Type a message"]')
-
-## type message in range
-for i in range(count):
+    ## type message in range
     print()
     print("Message sending....")
     print()
     msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_4sWnG')
+    button = driver.find_element_by_class_name('_3HQNh ')
     button.click()
 
     print()
